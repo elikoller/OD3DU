e48b53f0d3d9e2e7c7f94730bfef0aa069725b9f
Elena Koller <kollerelena02@gmail.com>
2024-08-31 13:21:59 +0200

    started working on the computation file: general structure okidoki but the implementation for the concrete accuracies is missing

--- evaluation/computation.py
+++ evaluation/computation.py
@@ -39,7 +39,6 @@ class Evaluator():
     def __init__(self, cfg, split):
         self.cfg = cfg
         # 3RScan data info
-        ## sgaliner related cfg
         self.split = split
         ## data dir
         self.data_root_dir = cfg.data.root_dir
@@ -48,6 +47,15 @@ class Evaluator():
         self.scans_files_dir = osp.join(self.scans_dir, 'files')
         self.scans_files_dir_mode = osp.join(self.scans_files_dir)
         self.scans_scenes_dir = osp.join(self.scans_dir, 'scenes')
+        self.inference_step = cfg.data.inference_step
+        #model info
+        self.model_name = cfg.model.name
+
+        #parameters
+        self.k_means = cfg.parameters.k_means
+        self.ths = cfg.parameters.threshold
+
+
         
 
 
@@ -105,51 +113,89 @@ class Evaluator():
         for scan_id in self.scan_ids:
             self.image_paths[scan_id] = self.load_frame_paths(self.scans_dir, scan_id)
 
-        # image preprocessing 
-        self.img_rotate = self.cfg.data.img_encoding.img_rotate
-        self.step = self.cfg.data.img.img_step
-        
-    
+      
         #output path for components
         self.out_dir_objects = osp.join(self.data_root_dir, "Results" )
         common.ensure_dir(self.out_dir_objects)
 
+     
+    def load_frame_idxs(self,data_dir, scan_id, skip=None):
+      
+        frames_paths = glob(osp.join(data_dir, scan_id, 'sequence', '*.jpg'))
+        frame_names = [osp.basename(frame_path) for frame_path in frames_paths]
+        frame_idxs = [frame_name.split('.')[0].split('-')[-1] for frame_name in frame_names]
+        frame_idxs.sort()
 
-    def compute(self, mode):
-        #loop over the different scenes
-        for scan_id in tqdm(self.scan_ids):
-            compute 
-        
-        python
-
-
-
-# Example usage
-if __name__ == "__main__":
-    # Define your scenes and frames
-    all_scenes = [list_of_frames_scene1, list_of_frames_scene2, ...]
+        if skip is None:
+            frame_idxs = frame_idxs
+        else:
+            frame_idxs = [frame_idx for frame_idx in frame_idxs[::skip]]
+        return frame_idxs
     
-    # Process all scenes and compute averages
-    overall_avg_matrix1, overall_avg_matrix2 = process_all_scenes(all_scenes)
+    def compute(self, mode):
+        #prepare the matricies for the 4 different metrics
+        all_cosine_obj = []
+        all_cosine_patch = []
+        all_euclid_obj = []
+        all_euclid_patch = []
+
+        #loop over each frame and append the resultsto the total matricies
+        for scan_id in tqdm(self.scan_ids):
+            # Load image paths and frame indices
+            frame_idxs_list = self.load_frame_idxs(self.scans_scenes_dir,scan_id)
+            frame_idxs_list.sort()
+            #access the necessary things for the reference scene
+            reference_id = scan3r.get_reference_id(self.data_root_dir, scan_id)
+            reference_info_path = osp.join("/media/ekoller/T7/Features2D/projection", self.model_name, "patch_{}_{}".format(self.image_patch_w,self.image_patch_h),"{}.pkl".format(scan_id))
+
+            with open(reference_info_path, 'rb') as file:
+                ref_data = pickle.load(file)
+
+            ref_obj_ids = np.array(ref_data["obj_ids"])
+            ref_proj_points = np.array(ref_data[mode]["points"])
+
+            #access the scan_info
+            scan_info_path = self.out_dir = osp.join("/media/ekoller/T7/Features2D/dino_segmentation", self.model_name, "patch_{}_{}".format(self.image_patch_w,self.image_patch_h),"{}.pkl".format(scan_id))
+            with open(scan_info_path, 'rb') as file:
+                scan_data = pickle.load(file)
+    
+
+            #now the frame
+            for infer_step_i in range(0, len(frame_idxs_list) // self.inference_step + 1):
+                start_idx = infer_step_i * self.inference_step
+                end_idx = min((infer_step_i + 1) * self.inference_step, len(frame_idxs_list))
+                frame_idxs_sublist = frame_idxs_list[start_idx:end_idx]
 
 
+                for frame_idx in frame_idxs_sublist:
+                    #initialze the matricies we will fill
+                    #get the lengths of the parameters
+                    ths_len = len(self.ths)
+                    k_means_len = len(self.k_means)
 
+                    cosine_obj = np.zeros((ths_len,k_means_len))
+                    cosine_patch = np.zeros((ths_len,k_means_len))
+                    euclid_obj = np.zeros((ths_len,k_means_len))
+                    euclid_patch = np.zeros((ths_len,k_means_len))
+                    #get the projected points in the correct mode
+                    frame_obj_ids = scan_data[frame_idx]["obj_ids"]
+                    frame_proj_points = scan_data[frame_idx][mode]["points"]
 
+                    #start the computations and iteraste through every combination
+                    for t_idy, th in enumerate (self.th):
+                        for k_idx, k in enumerate (self.k_means):
+                            #compute the result for the cosine dist
 
+                            #compute the result for the euclid dist
 
+                    
+                
+               
+              
 
-
-
-
-
-
-
-
-
-
-
-
-
+                  
+               
+    
 
 
 def parse_args():
