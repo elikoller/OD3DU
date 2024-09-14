corrected the statistic computation and also added debugged the trainloop for the object centers

matching/computation_statistics.py
@@ -109,7 +109,7 @@ class Evaluator():
         self.all_scans_split.sort()
 
         if self.rescan:
-            self.scan_ids = self.all_scans_split
+            self.scan_ids = self.all_scans_split[120:]
         else:
             self.scan_ids = ref_scans_split
 
@@ -121,7 +121,7 @@ class Evaluator():
 
     
         #output path for components
-        self.out_dir = osp.join(self.data_root_dir, "Results" )
+        self.out_dir = "/media/ekoller/T7/Results" #osp.join("/media/ekoller/T7", "Results")
         common.ensure_dir(self.out_dir)
 
      
@@ -228,7 +228,7 @@ class Evaluator():
         #get the ids over which we will iterate
         img_ids = np.unique(computed_patches)
         ious = []
-
+        
         #iterate over them
         for id in img_ids:
             #ids which are bigger than 0 are matched to the objects which were already in the scenegraph
@@ -240,7 +240,7 @@ class Evaluator():
             gt_ids_at_coords = gt_patches[tuple(coords.T)]
             #get the max id
             gt_max_id = Counter(gt_ids_at_coords).most_common(1)[0][0]
-
+            
             #if the gt id is 0 we have no info
             if gt_max_id != 0:
                 #check if it should be a new object 
@@ -249,30 +249,48 @@ class Evaluator():
                     if gt_max_id not in new_objects:
                         ious.append(0)
                         continue
+                    #case predicted new and gt_max also belongs to new obj
+                    #create the masks for the iou computeaiton
+                    #since the ids do not match filter the ids individually
+                    compued_mask = (computed_patches == id)
+                    gt_mask = (gt_patches == gt_max_id)
 
-                #create the masks for the iou computeaiton
-                compued_mask = (computed_patches == id)
-                gt_mask = (gt_patches == gt_max_id)
+                    #compute the iou
+                    iou_id = self.calculate_iou(compued_mask, gt_mask)
+                    ious.append(iou_id)
 
-                #compute the iou
-                iou_id = self.calculate_iou(compued_mask, gt_mask)
-                ious.append(iou_id)
-           
+                #we look at a seen object
+                else:
+                    #the predicted id does not match the gt so already wront
+                    if id != gt_max_id:
+                        ious.append(0)
+                        continue
+                    #the object ids overlap so find the iou
+                    #create the masks for the iou computeaiton
+                    compued_mask = (computed_patches == id)
+                    gt_mask = (gt_patches == gt_max_id)
+
+                    #compute the iou
+                    iou_id = self.calculate_iou(compued_mask, gt_mask)
+                    ious.append(iou_id)
         
+        if(len(ious) == 0):
+            return np.nan
 
-        return np.mean(ious)
+        return np.nanmean(ious)
 
 
     def compute_iou_metric_recall(self, gt_patches, computed_patches, new_objects):
         gt_ids  = np.unique(gt_patches)
         ious = []
-
+       
         #iterate over them
         for id in gt_ids:
             #only do actual objects - not 0 id
+           
             if id != 0:
                 #ids which are bigger than 0 are matched to the objects which were already in the scenegraph
-            
+     
                 #get the coordinates of this id
                 coords = np.argwhere(gt_patches == id)
 
@@ -280,23 +298,44 @@ class Evaluator():
                 computed_ids_at_coords = computed_patches[tuple(coords.T)]
                 #get the max id
                 computed_max_id = Counter(computed_ids_at_coords).most_common(1)[0][0]
-                #check if it should be a new object 
+                #check if it should be a new object
+                # gt new obj 
+                
                 if id in new_objects:
-                    #if is new object but did not get detected
+                    #if is new object but did not get predicted as unseen
                     if computed_max_id > 0:
+                      
                         ious.append(0)
                         continue
 
-                #create the masks for the iou computeaiton
-                gt_mask = (gt_patches == id)
-                computed_mask = (computed_patches == computed_max_id)
+                    #create the masks for the iou computeaiton
+                    gt_mask = (gt_patches == id)
+                    computed_mask = (computed_patches == computed_max_id)
 
-                #compute the iou
-                iou_id = self.calculate_iou(gt_mask, computed_mask)
-                ious.append(iou_id)
-           
+
+                    #compute the iou
+                    iou_id = self.calculate_iou(gt_mask, computed_mask)
+                   
+                    ious.append(iou_id)
+                #we are talking about a seen object
+                else:
+                    #not the correct predicted id
+                    if id != computed_max_id:
+                      
+                        ious.append(0)
+                        continue
+                     #create the masks for the iou computeaiton
+                    gt_mask = (gt_patches == id)
+                    computed_mask = (computed_patches == computed_max_id)
+
+                    #compute the iou
+                    iou_id = self.calculate_iou(gt_mask, computed_mask)
+                  
+                    ious.append(iou_id)
+
+        if(len(ious) == 0):
+            return np.nan
         
-
         return np.mean(ious)
 
 
@@ -560,12 +599,16 @@ class Evaluator():
                         cosine_iou_metric_precision[t_idx][k_idx] = precision
                         cosine_iou_metric_recall[t_idx][k_idx] = recall
                         f1 = 0 #formula 2*(precision*recall)/(precision+recall)
-                        if (precision+ recall) == 0:
-                            f1 = 0.0
-                        else:
-                            f1 = 2 * (precision * recall) / (precision + recall)
+                        if (not np.isnan(precision)) and (not np.isnan(recall)): 
+                            if (precision+ recall) == 0:
+                                f1 = 0.0
+                            else:
+                                f1 = 2 * (precision * recall) / (precision + recall)
 
-                        cosine_metric_f1[t_idx][k_idx] = f1
+                            cosine_metric_f1[t_idx][k_idx] = f1
+                        #we got a nan value
+                        else:
+                            cosine_metric_f1[t_idx][k_idx] = np.nan
            
                 scan_cosine_iou_metric_precision.append(cosine_iou_metric_precision)
                 scan_cosine_iou_metric_recall.append(cosine_iou_metric_recall)
@@ -575,7 +618,7 @@ class Evaluator():
 
             
                         
-        return  scan_cosine_iou_metric_precision, scan_cosine_iou_metric_recall, scan_cosine_metric_f1
+        return  np.nanmean(scan_cosine_iou_metric_precision,axis=0), np.nanmean(scan_cosine_iou_metric_recall,axis=0), np.nanmean(scan_cosine_metric_f1,axis=0)
 
     def compute(self, mode):
         #prepare the matricies for the 4 different metrics
@@ -583,33 +626,41 @@ class Evaluator():
         all_cosine_iou_metric_recall = []
         all_cosine_iou_metric_f1 = []
        
-     
-
-        workers = 2
-        
-        # parallelize the computations
-        with concurrent.futures.ProcessPoolExecutor(max_workers= workers) as executor:
-            futures = {executor.submit(self.compute_scan, scan_id, mode): scan_id for scan_id in self.scan_ids}
+    
+        # Use tqdm for progress bar, iterating as tasks are completed
+        with tqdm(total=len(self.scan_ids)) as pbar:
+            for scan_id in self.scan_ids:
+                print("scanid", scan_id)
+                cosine_iou_metric_precision, cosine_iou_metric_recall, cosine_metric_f1 = self.compute_scan(scan_id,mode)
+                
+                # get the result matricies
+                all_cosine_iou_metric_precision.append(cosine_iou_metric_precision)
+                all_cosine_iou_metric_recall.append(cosine_iou_metric_recall)
+                all_cosine_iou_metric_f1.append(cosine_metric_f1)
+                print("added results of scan id ", scan_id, " successfully")
             
-            # Use tqdm for progress bar, iterating as tasks are completed
-            with tqdm(total=len(self.scan_ids)) as pbar:
-                for future in concurrent.futures.as_completed(futures):
-                    scan_id = futures[future]
-                    try:
-                        cosine_iou_metric_precision, cosine_iou_metric_recall, cosine_metric_f1 = future.result()
+                
+                # progressed
+                pbar.update(1)
+
+            # with tqdm(total=len(self.scan_ids)) as pbar:
+            #     for future in concurrent.futures.as_completed(futures):
+            #         scan_id = futures[future]
+            #         try:
+            #             cosine_iou_metric_precision, cosine_iou_metric_recall, cosine_metric_f1 = future.result()
                         
-                       # get the result matricies
-                        all_cosine_iou_metric_precision.extend(cosine_iou_metric_precision)
-                        all_cosine_iou_metric_recall.extend(cosine_iou_metric_recall)
-                        all_cosine_iou_metric_f1.extend(cosine_metric_f1)
-                        print("added results of scan id ", scan_id, " successfully")
-                    except Exception as exc:
-                        print(f"Scan {scan_id} generated an exception: {exc}")
-                        print("Traceback details:")
-                        traceback.print_exc()
+            #            # get the result matricies
+            #             all_cosine_iou_metric_precision.append(cosine_iou_metric_precision)
+            #             all_cosine_iou_metric_recall.append(cosine_iou_metric_recall)
+            #             all_cosine_iou_metric_f1.append(cosine_metric_f1)
+            #             print("added results of scan id ", scan_id, " successfully")
+            #         except Exception as exc:
+            #             print(f"Scan {scan_id} generated an exception: {exc}")
+            #             print("Traceback details:")
+            #             traceback.print_exc()
                     
-                    # progressed
-                    pbar.update(1)
+            #         # progressed
+            #         pbar.update(1)
 
         # new_obj = []
         # for scan_id in tqdm(self.scan_ids, desc="Processing Scans"):
@@ -631,24 +682,24 @@ class Evaluator():
 
         print("writing the file")
         #we want the result over all scenes
-        mean_cosine_iou_metric_precision = np.mean(np.array(all_cosine_iou_metric_precision), axis=0)
-        mean_cosine_iou_metric_recall = np.mean(np.array(all_cosine_iou_metric_recall), axis=0)
-        mean_cosine_metric_f1 = np.mean(np.array(all_cosine_iou_metric_f1), axis= 0)
+        # mean_cosine_iou_metric_precision = np.mean(np.array(all_cosine_iou_metric_precision), axis=0)
+        # mean_cosine_iou_metric_recall = np.mean(np.array(all_cosine_iou_metric_recall), axis=0)
+        # mean_cosine_metric_f1 = np.mean(np.array(all_cosine_iou_metric_f1), axis= 0)
       
-        print("precision", mean_cosine_iou_metric_precision)
-        print("recall", mean_cosine_iou_metric_recall) 
-        print("F1 score", mean_cosine_metric_f1)     
+        # print("precision", mean_cosine_iou_metric_precision)
+        # print("recall", mean_cosine_iou_metric_recall) 
+        # print("F1 score", mean_cosine_metric_f1)     
 
         #create sesult dict
-        result = {"cosine_iou_metric_precision": mean_cosine_iou_metric_precision,
-                  "cosine_iou_metric_recall": mean_cosine_iou_metric_recall,
-                  "cosine_mectric_f1": mean_cosine_metric_f1
+        result = {"cosine_iou_metric_precision": all_cosine_iou_metric_precision,
+                  "cosine_iou_metric_recall": all_cosine_iou_metric_recall,
+                  "cosine_mectric_f1": all_cosine_iou_metric_f1
                 }
                   
         #save the file in the results direcrtory
         result_dir = osp.join(self.out_dir,mode)
         common.ensure_dir(result_dir)
-        result_file_path = osp.join(result_dir,  "statistics_segmentation.pkl")
+        result_file_path = osp.join(result_dir,  "statistics_segmentation120to180.pkl")
         common.write_pkl_data(result, result_file_path)
                     
                 

sceneGraph_update/generate_obj_centers_train.py
@@ -313,7 +313,7 @@ class Evaluator():
       #for a given scene get the colours of the differnt object_ids
     def get_present_obj_ids(self, data_dir,scan_id):
         #access the mesh file to get the colour of the ids
-        mesh_file = osp.join(data_dir,"scenes", scan_id, "labels.instances.annotated.v2.ply")
+        mesh_file = osp.join(data_dir,"scenes", scan_id, "labels.instances.align.annotated.v2.ply")
         ply_data = plyfile.PlyData.read(mesh_file)
         # Extract vertex data
         vertices = ply_data['vertex']
@@ -396,8 +396,9 @@ class Evaluator():
                     dino_id = boundingboxes["object_id"]
                     #print("frame ", frame_idx, " dino_id ", dino_id)
                     #get the matched id
-                    #print("matches frame ", frame_matches)
                     object_id = frame_matches[str(dino_id)]
+                    #print("matched id ", object_id)
+                    
                     
 
                     #isolate only the object pointcloud
@@ -406,6 +407,7 @@ class Evaluator():
                     if object_id > 0:
                         #now we need to find out if we add it to the pointcloud of the object it mapped to or not
                         if object_id not in all_clusters:
+                            #print("create first cluter obj_id ", object_id)
                             #there are no clusters & votes stored for this object jet
                             all_clusters[object_id] = [{'cluster': obj_pcl, 'votes': 1}]
                         #object already has pointclouds we need to see if we merge or add a new cluster
@@ -488,16 +490,18 @@ class Evaluator():
 
 
 
-
+        print("clusters keys", all_clusters.keys())
+        print("all clusters", all_clusters)
         #now that we have the lists of clusters we need to iterate over them and choose the biggest cluster, downsample it & take the average to predict the center
         #initialize final object
         all_centers = {}
         #iterte through the objects
-        for obj_id, clusters in enumerate(all_clusters):
+        for obj_id, clusters in all_clusters.items():
+            print("in the for loop with cluster id", obj_id)
             #get the cluster with the most points aka largest 
-            print(clusters)
+            print("clusters", clusters , "for object id " ,obj_id)
             #decide the most likely correct cluster based on votes first and then size
-            largest_cluster_data = max(all_clusters[object_id], key=lambda c: (c['votes'], len(c['cluster'])))
+            largest_cluster_data = max(all_clusters[obj_id], key=lambda c: (c['votes'], len(c['cluster'])))
             largest_cluster = largest_cluster_data['cluster']
             largest_cluster_votes = largest_cluster_data["votes"]
             #create the objec center
@@ -512,7 +516,7 @@ class Evaluator():
                 "points": len(largest_cluster),
                 "votes" : largest_cluster_votes
             }
-            #print(all_centers[0])
+            print(all_centers)
 
             return all_centers
  
@@ -521,8 +525,11 @@ class Evaluator():
     def is_in_boundingbox(self, center, boundingbox):
         min_coords = np.min(boundingbox, axis=0)
         max_coords = np.max(boundingbox, axis=0)
+        is_inside = (np.all(min_coords <= center) and np.all(center <= max_coords))
 
-        return ((min_coords[0] <= center[0] <= max_coords[0]) and (min_coords[1] <= center[1] <= max_coords[1]) and (min_coords[2] <= center[2] <= max_coords[2]))
+        if is_inside:
+            print("the object was inside")
+        return is_inside
 
 
 
@@ -578,6 +585,7 @@ class Evaluator():
                     if (votes >= min_vote) and (points >= num_points):
                         predicted[obj_id] = obj_data
 
+              
                 matched_predicted_ids = set()
                 """ 
                 add logiv for newly seeno objects
@@ -662,6 +670,9 @@ class Evaluator():
                 recalls[j][i] = recall
                 f1s[j][i] =  f1_score
 
+        print("precision",precisions)
+        print("recall",recalls)
+        print("f1 scores", f1s)
         return precisions,recalls,f1s
           
 
@@ -687,9 +698,9 @@ class Evaluator():
                         precision,recall, f1 = future.result()
                         
                        # get the result matricies
-                        all_precision.extend(precision)
-                        all_recall.extend(recall)
-                        all_f1.extend(f1)
+                        all_precision.append(precision)
+                        all_recall.append(recall)
+                        all_f1.append(f1)
                         print("added results of scan id ", scan_id, " successfully")
                     except Exception as exc:
                         print(f"Scan {scan_id} generated an exception: {exc}")
@@ -719,9 +730,9 @@ class Evaluator():
 
         print("writing the file")
         #we want the result over all scenes
-        mean_precision = np.mean(np.array(all_precision), axis=0)
-        mean_recall = np.mean(np.array(all_recall), axis=0)
-        mean_f1 = np.mean(np.array(all_f1), axis= 0)
+        mean_precision = np.mean(all_precision, axis=0)
+        mean_recall = np.mean(all_recall, axis=0)
+        mean_f1 = np.mean(all_f1, axis= 0)
       
         print("precision", mean_precision)
         print("recall", mean_recall) 